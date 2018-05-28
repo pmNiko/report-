1efe08d68789f2ea1de941e18709f04a5a64836c
Nikolas <nikolas090189@gmail.com>
2018-05-28 20:10:17 -0300

    fix error cookies for safari 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery 
+  skip_before_action :verify_authenticity_token, :except => [:update, :create]
   before_action :authenticate_user!
 end
