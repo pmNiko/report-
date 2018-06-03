5df395dfbac872b0076fb2e2b0350c731be82bf5
toriak <raulfvidela@gmail.com>
2018-06-03 17:08:43 -0300

    create model events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_151446) do
+ActiveRecord::Schema.define(version: 2018_06_03_200452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2018_05_29_151446) do
   create_table "claims_users", force: :cascade do |t|
     t.integer "claim_id"
     t.integer "user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "star_time"
+    t.datetime "end_time"
+    t.integer "type_event"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "roles", force: :cascade do |t|
