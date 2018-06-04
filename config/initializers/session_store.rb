2aa07613b69c70dc9de3274f2e06496407ac8105
Nikolas <nikolas090189@gmail.com>
2018-06-04 16:35:22 -0300

    Config session_store.rb 2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-#Rails.application.config.session_store :cookie_store, key: '_report-_session'
+Rails.application.config.session_store :cookie_store, key: '_report-_session'
 Rails.application.config.session_store :cookie_store, expire_after: 14.days
