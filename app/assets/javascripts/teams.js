894fd3bc76d2c272a7cfc9b77dc649cc570f61ba
Nikolas <nikolas090189@gmail.com>
2018-07-03 02:24:53 -0300

    Clock added

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -29,6 +29,27 @@ $(document).on('turbolinks:load', function () {
       'August', 'September', 'Octuber', 'November', 'Dicember'
     ];
 
+    if (hours >= 12) {
+      hours = hours - 12;
+      ampm = 'PM';
+    }else {
+      ampm = 'AM'
+    }
+
+    if (hours == 0) {
+      hours = 12;
+    };
+
+    if (hours < 10) {
+      hours = "0" + hours
+    };
+    if (minutes < 10) {
+      minutes = "0" + minutes
+    };
+    if (seconds < 10) {
+      seconds = "0" + seconds
+    };
+
     pWeekendDay.textContent = weekend[weekendDay];
     pDay.textContent = day;
 
@@ -36,9 +57,12 @@ $(document).on('turbolinks:load', function () {
     pYear.textContent = year;
     pHours.textContent = hours;
     pMinutes.textContent = minutes;
-    
+    pAMPM.textContent = ampm;
+    pSeconds.textContent = seconds;
+
   };
 
   updateHour();
+  var interval = setInterval(updateHour, 1000);
 
 });
